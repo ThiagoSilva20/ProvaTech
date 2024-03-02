cfc060f5be9e6b635afc70136228064abbaef6bf
ThiagoSilva20 <thiago201714@gmail.com>
2024-03-02 16:23:41 -0300

    index pronto e fazendo cardapio

--- 04_-_restaurante-oriental-com-js/cardapio.js
+++ 04_-_restaurante-oriental-com-js/cardapio.js
@@ -1,10 +1,42 @@
-let formulario = document.getElementById("form");
-formulario.style.color = "white";
-formulario.style.fontSize = '20px';
-formulario.style.backgroundColor = 'rgba(0,0,0,0.4)';
-formulario.style.padding = '5px';
-formulario.style.borderRadius = '5px';
+//aplicando estilo no html
 
+//tag form
+let formulario = document.getElementById("form");
+formulario.style.display = "block";
+formulario.style.width = "220px";
+formulario.style.padding = "10px";
+
+//tag div
 let form = document.getElementById("paiform");
-form.style.display = 'flex';
-form.style.alignItems = 'center';
+form.style.color = "white";
+form.style.fontSize = '20px';
+form.style.backgroundColor = 'rgba(0,0,0,0.4)';
+form.style.borderRadius = '5px';
+form.style.display = "flex";
+form.style.textAlignLast = 'center';
+form.style.width = "220px"
+form.style.margin = "0 auto"
+
+
+//tag h4
+let titulopromo = document.getElementById("titulopromo");
+titulopromo.style.margin = 0;
+
+//tag button
+let botaopromo = document.getElementById("botaopromo");
+botaopromo.style.border = "none";
+botaopromo.style.borderRadius = '5px';
+botaopromo.style.width = '70px';
+botaopromo.style.height = '30px';
+botaopromo.style.marginTop = '6px';
+botaopromo.style.background = "rgba(255, 255, 255, 0.1)";
+
+//tag input
+let email = document.getElementById("email");
+email.style.background = "rgba(255,255,255,0.1)";
+email.style.borderRadius = "5px";
+email.style.width = "80%";
+email.style.height = "30%";
+email.style.border = "none";
+email.style.color = "white";
+
