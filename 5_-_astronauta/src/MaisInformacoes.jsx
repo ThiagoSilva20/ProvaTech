a0f8afee51ffd705e0fb45aedee8ed64a6614c6a
ThiagoSilva20 <thiago201714@gmail.com>
2024-04-21 01:34:43 -0300

    Projeto Final Tech4me

--- 5_-_astronauta/src/MaisInformacoes.jsx
+++ 5_-_astronauta/src/MaisInformacoes.jsx
@@ -1,22 +1,44 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { useParams } from "react-router-dom"
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams, Link } from "react-router-dom"
+import axios from 'axios'
+import './style/MaisInformacoes.css'
 
-const MaisInformacoes = () => {
+const DetalheAsteroide = () => {
   const { id } = useParams()
-  const [asteroide, setAsteroide] = useState(null);
-  useEffect(async () => {
-      const response = await axios.get(
-        `https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=QWRE3ULoOtEeoaXwFMb1WDwWrY4Muv7NncJFWWsn`
-      )
-      
-        setAsteroide(response.data);
-    }, [])
-    return (
-    <>
-    {asteroide.map((astro) => (
-      <h2 key={astro.id}>{astro.title}</h2>
-    ))}</>
-    )}
+  const [asteroide, setAsteroide] = useState(null)
 
-export default MaisInformacoes
+  const requisicao = async () => {
+    const asteroideConsulta = await axios.get(
+      `https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=QWRE3ULoOtEeoaXwFMb1WDwWrY4Muv7NncJFWWsn`
+    )
+    setAsteroide(asteroideConsulta.data)
+
+  }
+  useEffect(() => {
+    requisicao()
+  }, [])
+
+
+  return (
+    <>
+      <div id='detalhes_div'>
+        <h1>Detalhes do asteroide</h1>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{asteroide?.name}</h5>
+            <h6>É potencialmente perigoso?<br />{asteroide?.is_potentially_hazardous_asteroid ? 'Sim' : 'Não'}</h6>
+            <h6>Diâmetro Máximo<br />{asteroide?.estimated_diameter.kilometers.estimated_diameter_max}</h6>
+            <h6>Velocidade relativa(Hora)<br />{asteroide?.close_approach_data?.[0]?.relative_velocity?.kilometers_per_hour}</h6>
+            <h6>Magnitude absoluta<br />{asteroide?.absolute_magnitude_h}</h6>
+            <Link style={{border:'1px solid black', borderRadius:'5px', textDecoration:'none', color:'black', padding:'5px 10px', background:'rgba(211, 211, 211, 0.5)'}} to={asteroide?.nasa_jpl_url}>Mais Detalhes</Link>
+          </div>
+        </div>
+      </div >
+    </>
+  )
+}
+
+
+
+export default DetalheAsteroide;
--- 5_-_astronauta/src/main.jsx
+++ 5_-_astronauta/src/main.jsx
@@ -5,16 +5,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PageInicial from './PageInicial.jsx'
 import MaisInformacoes from './MaisInformacoes.jsx'
 import Sobre from './SobreMim.jsx'
-import Nav from './Nav.jsx'
+import ListaAsteroids from './ListaAsteroides.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Nav />
+   
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PageInicial />}/>
-        <Route path="/asteroides:id" element={<MaisInformacoes />} />
-        <Route path="/sobre-mim" element={<Sobre />}/>
+        <Route path="/" element={<PageInicial />}>
+            <Route path="/" element={<ListaAsteroids />} />
+            <Route path="/asteroides/:id" element={<MaisInformacoes />} />
+            <Route path="/sobre-mim" element={<Sobre />}/>
+        </Route>
       </Routes>
     
     </BrowserRouter>
